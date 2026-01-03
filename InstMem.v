c3f88fac3e4c044443703d59f4966cd8a9017fa5
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2026-01-03 22:01:10 +0330

    InsMem

--- InstMem.v
+++ InstMem.v
@@ -1,11 +1,10 @@
-module InstMemory (input[31:0] A,output[31:0] RD); 
-
+module InstMemory (
+    input[31:0] A,
+    output[31:0] RD
+    ); 
     reg [7:0] instMem [0:$pow(2, 16)-1]; 
-
     wire [31:0] adr;
     assign adr = {A[31:2],2'b00}; 
-    
-
     initial $readmemh("inst_memory.mem", instMem);
     assign RD = {instMem[adr+3], instMem[adr + 2], instMem[adr + 1], instMem[adr]};
 endmodule
