2a4284cf8c4341934f9a4cd9799bfe50b0759208
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2026-01-03 22:05:07 +0330

    Register

--- Register.v
+++ Register.v
@@ -1,8 +1,14 @@
-module Register(clk,en,sclr,in,out);
+module Register(
+	clk,en,sclr,in,out
+	);
+
+
 	parameter N=32;
 	input clk,en,sclr;
 	input[N-1:0] in;
 	output reg[N-1:0] out={N{1'b0}};
+
+	
 	always@(posedge clk)begin
 		if(sclr)
 		    	out<={N{1'b0}};
