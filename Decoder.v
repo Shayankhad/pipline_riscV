790200b76694ecf4a81933fe8b8c1c4da230c448
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2026-01-04 10:44:30 +0330

    Decoder

--- Decoder.v
+++ Decoder.v
@@ -1,18 +1,14 @@
 module decoder(
-	input JalrE,JumpE,BranchE,ZeroE,neg,
-	input[2:0] func3,output[1:0] PCSrc
-	
-	);
-	assign PCSrc = ( JalrE )  ?  2'b10:
-		((JumpE)||
-		( BranchE && func3== 3'b000 && ZeroE)|| // beq
+    input JalrE, JumpE, BranchE, ZeroE,
+    input [2:0] func3,
+    output [1:0] PCSrc
+);
 
-		( BranchE && func3  ==3'b001 && ~ZeroE)|| // bne
+assign PCSrc = (JalrE) ? 2'b10 :
+               ( (JumpE) ||
+                 (BranchE && func3 == 3'b000 && ZeroE) ||   // beq
+                 (BranchE && func3 == 3'b001 && ~ZeroE)     // bne
+               ) ? 2'b01 :
+               2'b00;
 
-		( BranchE && func3 == 3'b100 && neg)|| //blt
-
-		( BranchE && func3 == 3'b101 && ~neg))?2'b01: //bge
-
-		2'b00;
 endmodule
-
